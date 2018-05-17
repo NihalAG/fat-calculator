c2917fc5f7fd11004ea7cd775b29349f6ddca754
NihalAG <39355479+NihalAG@users.noreply.github.com>
2018-05-17 18:19:27 -0400

    First Commit

---
+++ FatCalc.cs
@@ -0,0 +1,167 @@
+﻿//Apoorva Solanki
+//    Harkeerat Kaur
+//    Kiranpreet Kaur
+//    Jiwant Singh
+//    Nihal Ahmed Gesudraz
+
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace FatPercentageCalculator
+{
+    class FatCalc : INotifyPropertyChanged
+    {
+        string ErrMsg, ErrMsgTwo;
+        Decimal noOfCal;
+        Decimal CalPctage; // for the calculating percentage 
+        Decimal calPct; // to calculate angle
+        Decimal calFromFat;
+        String meterValue;
+        String angle = "-120";
+        bool checkedStatus;
+
+        public Decimal NoOfCal
+        {
+            get
+            {
+                return noOfCal;
+            }
+            set
+            {
+                noOfCal = GetNumberOfCalories(value);
+                propertyChanged();
+            }
+        }
+
+        public Decimal CalPct
+        {
+            get
+            {
+                return calPct;
+            }
+            set
+            {
+                calPct = GetPercentageOfCaloriesFromFat(value);
+                propertyChanged();
+            }
+        }
+
+        public string ErrorMessage
+        {
+            get
+            {
+                return ErrMsg;
+            }
+            set
+            {
+                ErrMsg = value;
+                propertyChanged();
+            }
+        }
+
+        public string ErrorMessageTwo
+        {
+            get
+            {
+                return ErrMsgTwo;
+            }
+            set
+            {
+                ErrMsgTwo = value;
+                propertyChanged();
+            }
+        }
+
+        public String Angle
+        {
+            get
+            {
+                return angle;
+            }
+
+            set
+            {
+                angle = CalcAngle().ToString();
+                propertyChanged();
+            }
+        }
+
+        public String MeterValue
+        {
+            get
+            {
+                return meterValue;
+            }
+            set
+            {
+                meterValue = this.LowFatCheck();
+                propertyChanged();
+            }
+        }
+
+        public bool CheckedStatus
+        {
+            get
+            {
+                return checkedStatus;
+            }
+            set
+            {
+                checkedStatus = value;
+            }
+        }
+
+        private Decimal GetNumberOfCalories(Decimal gramsFat)
+        {
+            calFromFat = gramsFat * 9;
+            return calFromFat;
+        }
+
+        private Decimal GetPercentageOfCaloriesFromFat(Decimal TotCals)
+        {
+            //GramsOfFat Gives Zero On Start. 
+            //This just sets the value to 1. So we do not get a divide by zero error.
+            if (TotCals == 0)
+                TotCals = 1;
+            CalPctage = Math.Round((calFromFat / TotCals), 2) * 100;
+            return CalPctage;
+        }
+
+        public String LowFatCheck()
+        {
+            if (CalPctage <= 30)
+            {
+                return CalPctage.ToString() + "%"+(CheckedStatus == true? " Low Fat":"");
+            }
+            return CalPctage.ToString() + "%";
+        }
+
+        public String CalcAngle()
+        {
+            Decimal angle = ((CalPctage / 100) * 240) - 120;
+            return angle.ToString();
+        }
+
+        //Checks If It Is a Number Or A Decimal Using Regular Expressions
+        public Boolean IsValidPositiveNonBlankValue(string Val)
+        {
+            return Regex.IsMatch(Val, @"^[0-9,\.]+$");
+        }
+
+        public Boolean IsValueGreaterThanCalories(string Val)
+        {
+            Decimal TotCalories;
+            Decimal.TryParse(Val, out TotCalories);
+            return (TotCalories >= calFromFat) ? true : false;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void propertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿    //Apoorva Solanki
+    //Harkeerat Kaur
+    //Kiranpreet Kaur
+    //Jiwant Singh
+    //Nihal Ahmed Gesudraz
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using MaterialDesignThemes.Wpf;
+
+namespace FatPercentageCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        FatCalc fc = new FatCalc();
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            DataContext = fc;
+        }
+
+        private void Reset(object sender, RoutedEventArgs e)
+        {
+            TxtGof.Text = "";
+            TxtCalTot.Text = "";
+            fc.NoOfCal = 0;
+            fc.CalPct = 0;
+            fc.Angle = "-120";
+            fc.MeterValue = "";
+            DisplayErrorMessage("");
+            DisplaySecondErrorMessage("");
+            ChkCalcLow.IsChecked = false;
+        }
+
+        //Calculates the percentage.
+        private void Calculate(object sender, RoutedEventArgs e)
+        {
+            Button TxtCal = sender as Button;
+            Console.WriteLine(TxtCalTot.Text);
+            if (!fc.IsValidPositiveNonBlankValue(TxtGof.Text))
+            {
+                DisplayErrorMessage("Enter Non Zero Positive Values.");
+            }
+
+            if (!fc.IsValidPositiveNonBlankValue(TxtCalTot.Text))
+            {
+                DisplaySecondErrorMessage("Enter Non Zero Positive Values.");
+
+            }
+            else if (!fc.IsValueGreaterThanCalories(TxtCalTot.Text))
+            {
+                DisplaySecondErrorMessage("Invalid Total Fat Calories.");
+            }
+            else
+            {
+                fc.CalPct = Convert.ToDecimal(TxtCalTot.Text);
+                fc.Angle = fc.CalcAngle();
+                fc.MeterValue = fc.LowFatCheck();
+                DisplaySecondErrorMessage("");
+            }
+        }
+
+        private void IsFatNeeded(object sender, RoutedEventArgs e)
+        {
+            fc.CheckedStatus = true;
+        }
+
+        private void IsFatNotNeeded(object sender, RoutedEventArgs e)
+        {
+            fc.CheckedStatus = false;
+        }
+
+        private void CalcFatCal(object sender, RoutedEventArgs e)
+        {
+            TextBox TxtFat = sender as TextBox;
+            if (fc.IsValidPositiveNonBlankValue(TxtFat.Text))
+            {
+                fc.NoOfCal = Convert.ToDecimal(TxtFat.Text);
+                DisplayErrorMessage("");
+            }
+            else
+            {
+                DisplayErrorMessage("Enter Non Zero Positive Values.");
+            }
+        }
+
+        private void ValidateFatCal(object sender, RoutedEventArgs e)
+        {
+            if (!fc.IsValidPositiveNonBlankValue(TxtCalTot.Text))
+            {
+                DisplaySecondErrorMessage("Enter Non Zero Positive Values.");
+            }
+            else if (!fc.IsValueGreaterThanCalories(TxtCalTot.Text))
+            {
+                DisplaySecondErrorMessage("Invalid Total Fat Calories.");
+            }
+            else
+            {
+                DisplaySecondErrorMessage("");
+            }
+        }
+
+        private void DisplayErrorMessage(string Message)
+        {
+            fc.ErrorMessage = Message;
+        }
+
+        private void DisplaySecondErrorMessage(string Message)
+        {
+            fc.ErrorMessageTwo = Message;
+        }
+    }
+}
